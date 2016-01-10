ad5daa65754ee0b03496dbfea54f19d996df5b9a
Onekone <alexs0929@gmail.com>
2016-01-10 20:22:06 +0700

    Release 1.0
    Release 1.0

--- src/main/java/groupPadlock/App.java
+++ src/main/java/groupPadlock/App.java
@@ -6,13 +6,40 @@ import org.apache.log4j.Logger;
  * Hello world!
  */
 public class App {
-    final static Logger logger = Logger.getLogger(App.class);
+    final static Logger logger = Logger.getLogger (App.class);
 
-    public static void logDebug(String str){if(logger.isDebugEnabled()){logger.debug(str);}}
-    public static void logInfo(String str){if(logger.isInfoEnabled()){logger.info(str);}}
-    public static void logError(String str){logger.error(str);}
-    public static void logWarn(String str){logger.warn(str);}
-    public static void logFatal(String str){logger.fatal(str);}
+    public static void logDebug(String str) {
+        if (logger.isDebugEnabled ()) {
+            logger.debug (str);
+        }
+    }
+
+    public static void logInfo(String str) {
+        if (logger.isInfoEnabled ()) {
+            logger.info (str);
+        }
+    }
+
+    public static void logError(String str) {
+        logger.error (str);
+    }
+
+    public static void logWarn(String str) {
+        logger.warn (str);
+    }
+
+    public static void logFatal(String str) {
+        logger.fatal (str);
+    }
+
+
+    public static int hash(String word) {// per http://stackoverflow.com/questions/2624192/good-hash-function-for-strings
+        int hashValue = 7;
+        for (int i = 0; i < word.length (); i++) {
+            hashValue = hashValue * 31 + word.charAt (i);
+        }
+        return hashValue;
+    }
 
     static java.util.HashMap<String, String> loginSalt = new java.util.HashMap<String, String> ();
     static java.util.HashMap<String, Integer> loginInformation = new java.util.HashMap<String, Integer> ();
@@ -24,13 +51,13 @@ public class App {
 
     public static java.time.LocalDate padlockString2Data(String s) {
         try {
-            if (s==null) throw new java.time.format.DateTimeParseException("duh","none",5);
+            if (s == null) throw new java.time.format.DateTimeParseException ("duh", "none", 5);
             java.time.format.DateTimeFormatter formatter = java.time.format.DateTimeFormatter.ofPattern ("uuuu-M-d");
             java.time.LocalDate date1 = java.time.LocalDate.parse (s, formatter);
-            logInfo("Supplied date: " + date1 + " - OK");
+            logInfo ("Supplied date: " + date1 + " - OK");
             return date1;
         } catch (java.time.format.DateTimeParseException e) {
-            logError("Invalid data information supplied");
+            logError ("Invalid data information supplied");
             return null;
         }
     }
@@ -49,69 +76,74 @@ public class App {
                 + "   -h             - This help page\n");
     }
 
-    public static void padlockExit(String logNote, int statusCode)
-    {
-        logFatal(logNote);
+    public static void padlockExit(String logNote, int statusCode) {
+        logFatal (logNote);
         returnCode = statusCode;
     }
 
     public static int padlockAuth(String login, String password) {
-        logDebug("Trying to sign in with : login:" + login + " pass:" + password);
-        int convertedPassword = (loginSalt.get (login) + Integer.toString (password.hashCode ())).hashCode ();
-        if (login.equals ("") || login.equals(null)) {
-            logError("Missing username");
+        logDebug ("Trying to sign in with : login:" + login + " pass:" + password);
+        int convertedPassword = hash (loginSalt.get (login) + Integer.toString (hash (password)));
+        if (login.equals ("") || login.equals (null)) {
+            logError ("Missing username");
             return 1;
         } // Missing login
-        else if (password.equals ("")|| password.equals(null)) {
-            logError("Missing password");
+        else if (password.equals ("") || password.equals (null)) {
+            logError ("Missing password");
             return 2;
         } // Missing password
         else if (! loginInformation.containsKey (login)) {
-            logError("No such user exists");
+            logError ("No such user exists");
             return 3;
         } // Invalid login
         else if (convertedPassword != loginInformation.get (login)) {
-            logError("Invalid password");
+            logError ("Invalid password");
             return 4;
         }
         // Invalid password
 
-        logInfo("Welcome, " + loginNames.get (login));
+        logInfo ("Welcome, " + loginNames.get (login));
         return 0;
     }
 
-    public static int padlockAccess(String login, String resource, String role){
-        String[] explode= resource.split("\\.");
-        for(int i=1;i<explode.length;i++) explode[i] = explode[i-1]+"."+explode[i];
+    public static int padlockAccess(String login, String resource, String role) {
+        String[] explode = resource.split ("\\.");
+        for (int i = 1; i < explode.length; i++) explode[i] = explode[i - 1] + "." + explode[i];
 
         int roleInt = 0;
 
-        switch (role.toLowerCase())
-        {
-            case "read": roleInt = 1; break;
-            case "write": roleInt = 2; break;
-            case "execute": roleInt = 4; break;
+        switch (role.toLowerCase ()) {
+            case "read":
+                roleInt = 1;
+                break;
+            case "write":
+                roleInt = 2;
+                break;
+            case "execute":
+                roleInt = 4;
+                break;
         }
 
-        for(int i=0;i<loginRoles.size();i++)
-        {
-            String lgn = loginRoles.get(i).login;
-            String res = loginRoles.get(i).path;
-            int rle = loginRoles.get(i).role;
-            for(int j=0;j<explode.length;j++)
-            {
-            if (res.equals(explode[j]) && roleInt == rle) {logInfo("Access granted"); return 0;};
+        for (int i = 0; i < loginRoles.size (); i++) {
+            String lgn = loginRoles.get (i).login;
+            String res = loginRoles.get (i).path;
+            int rle = loginRoles.get (i).role;
+            for (int j = 0; j < explode.length; j++) {
+                if (res.equals (explode[j]) && roleInt == rle) {
+                    logInfo ("Access granted");
+                    return 0;
+                }
+                ;
             }
         }
 
-        if (roleInt==0) return 2;
+        if (roleInt == 0) return 2;
         return 1;
     }
 
-    public static void main(String[] args)
-    {
-        int exitCode = go(args);
-        System.exit(exitCode);
+    public static void main(String[] args) {
+        int exitCode = go (args);
+        System.exit (exitCode);
     }
 
     public static int go(String[] args) {
@@ -122,31 +154,30 @@ public class App {
         loginNames.put ("jdoe", "John Doe");
         loginNames.put ("jrow", "Jane Row");
 
-        loginRoles.add( new Access("jdoe","a",1) );
-        loginRoles.add( new Access("jdoe","a.b",2) );
-        loginRoles.add( new Access("jrow","a.b.c",4) );
-        loginRoles.add( new Access("jdoe","a.b.c",4) );
+        loginRoles.add (new Access ("jdoe", "a", 1));
+        loginRoles.add (new Access ("jdoe", "a.b", 2));
+        loginRoles.add (new Access ("jrow", "a.b.c", 4));
+        loginRoles.add (new Access ("jdoe", "a.b.c", 4));
 
-        loginInformation.put ("jdoe", (loginSalt.get ("jdoe") + Integer.toString ("sup3rpaZZ".hashCode ())).hashCode ());
-        loginInformation.put ("jrow", (loginSalt.get ("jrow") + Integer.toString ("Qweqrty12".hashCode ())).hashCode ());
-        
+        loginInformation.put ("jdoe", hash (loginSalt.get ("jdoe") + Integer.toString (hash ("sup3rpaZZ"))));
+        loginInformation.put ("jrow", hash (loginSalt.get ("jrow") + Integer.toString (hash ("Qweqrty12"))));
+
         // Основная программа
         // ШАГ 0: Отсортировать данные
 
         // Почему я не использую CLI, несмотря на то, что он вроде бы был? Нет времени разбираться в его настройке
         // Да, это грязно, но на самом деле та библиотека сделала бы тоже самое, только с другими костылями, оно работает,
         // мне это нравится и, что самое главное, я не нарушаю условия требований
-        int returnCode = 0;
         boolean showHelpOnce = false;
 
         String argsFull = "";
-        for(int i=0;i<args.length;i++) argsFull += args[i]+" ";
-        logInfo("Supplied arguments: "+argsFull);
+        for (int i = 0; i < args.length; i++) argsFull += args[i] + " ";
+        logInfo ("Supplied arguments: " + argsFull);
 
         // Если ничего не произошло, то этот блок спокойно убьет программу, не прогоняя её дальше
         if (args.length <= 0) {
             showHelp ();
-            logInfo("Nothing happened");
+            logInfo ("Nothing happened");
             return 0;
             //System.exit(statusCode);
         }
@@ -157,91 +188,116 @@ public class App {
             switch (args[i]) {
                 case "-login":
                     padlockArguments.put ("login", args[i + 1]);
-                    logDebug("Supplied LOGIN field: "+args[i+1]);
+                    logDebug ("Supplied LOGIN field: " + args[i + 1]);
                     i++;
                     combo1 = combo1 | 1;
                     break;
                 case "-pass":
                     padlockArguments.put ("pass", args[i + 1]);
-                    logDebug("Supplied PASS field: "+args[i+1]);
+                    logDebug ("Supplied PASS field: " + args[i + 1]);
                     i++;
                     combo1 = combo1 | 2;
                     break;
                 case "-res":
                     padlockArguments.put ("res", args[i + 1]);
-                    logDebug("Supplied RES field: "+args[i+1]);
+                    logDebug ("Supplied RES field: " + args[i + 1]);
                     i++;
                     combo2 = combo2 | 1;
                     break;
                 case "-role":
                     padlockArguments.put ("role", args[i + 1]);
-                    logDebug("Supplied ROLE field: "+args[i+1]);
+                    logDebug ("Supplied ROLE field: " + args[i + 1]);
                     i++;
                     combo2 = combo2 | 2;
                     break;
                 case "-ds":
                     padlockArguments.put ("ds", args[i + 1]);
-                    logDebug("Supplied DATE START field: "+args[i+1]);
+                    logDebug ("Supplied DATE START field: " + args[i + 1]);
                     i++;
                     combo3 = combo3 | 1;
                     break;
                 case "-de":
                     padlockArguments.put ("de", args[i + 1]);
-                    logDebug("Supplied DATE END field: "+args[i+1]);
+                    logDebug ("Supplied DATE END field: " + args[i + 1]);
                     i++;
                     combo3 = combo3 | 2;
                     break;
                 case "-vol":
                     padlockArguments.put ("vol", args[i + 1]);
-                    logDebug("Supplied VOLUME field: "+args[i+1]);
+                    logDebug ("Supplied VOLUME field: " + args[i + 1]);
                     i++;
                     combo3 = combo3 | 4;
                     break;
                 default:
-                    if (!showHelpOnce) {
+                    if (! showHelpOnce) {
                         showHelpOnce = true;
                         showHelp ();
                     }
-                    if (!args[i].equals("-h")) logError("Unknown field "+args[i]+" . Aborting...\n Nothing happened");
+                    if (! args[i].equals ("-h"))
+                        logError ("Unknown field " + args[i] + " . Aborting...\n Nothing happened");
                     return 0;
             }
         }
 
-        if (combo1 != 3 && combo1 > 0) {logFatal("Not enough arguments supplied"); return 1;}
-        if (combo2 != 3 && combo2 > 0) {logFatal("Not enough arguments supplied"); return 3;}
-        if (combo3 != 7 && combo3 > 0) {logFatal("Not enough arguments supplied"); return 5;}
+        if (combo1 != 3 && combo1 > 0) {
+            logFatal ("Not enough arguments supplied");
+            return 1;
+        }
+        if (combo2 != 3 && combo2 > 0) {
+            logFatal ("Not enough arguments supplied");
+            return 3;
+        }
+        if (combo3 != 7 && combo3 > 0) {
+            logFatal ("Not enough arguments supplied");
+            return 5;
+        }
 
         // ШАГ 1: Логинимся
 
-        switch (padlockAuth (padlockArguments.get("login"), padlockArguments.get("pass"))) {
-            case 1:case 3:
-                logFatal("Error 1 - Unknown username "+padlockArguments.get("login"));
+        switch (padlockAuth (padlockArguments.get ("login"), padlockArguments.get ("pass"))) {
+            case 1:
+            case 3:
+                logFatal ("Error 1 - Unknown username " + padlockArguments.get ("login"));
                 return 1;
-            case 2:case 4:
-                logFatal("Error 2 - Incorrect password "+padlockArguments.get("pass"));
+            case 2:
+            case 4:
+                logFatal ("Error 2 - Incorrect password " + padlockArguments.get ("pass"));
                 return 2;
         }
 
         // ШАГ 2: Узнаем данные о ресурсе
-        if (padlockArguments.containsKey("role") && padlockArguments.containsKey("res"))
-        switch ( padlockAccess( padlockArguments.get("login") , padlockArguments.get("res") , padlockArguments.get("role") ) )
-        {
-            case 2: logFatal("Error 3 - Invalid role supplied"); return 3;
-            case 1: logFatal("Error 4 - No access"); return 4;
-        }
-        else logWarn("No access information, skipping");
+        if (padlockArguments.containsKey ("role") && padlockArguments.containsKey ("res"))
+            switch (padlockAccess (padlockArguments.get ("login"), padlockArguments.get ("res"), padlockArguments.get ("role"))) {
+                case 2:
+                    logFatal ("Error 3 - Invalid role supplied");
+                    return 3;
+                case 1:
+                    logFatal ("Error 4 - No access");
+                    return 4;
+            }
+        else logWarn ("No access information, skipping");
 
         // ШАГ 3: Заполняем данные о дате и объеме
-        if (padlockArguments.containsKey("ds"))
-        {if (padlockString2Data(padlockArguments.get("ds")) == null) {logFatal("Error 5 - Incorrect date supplied");return 5;}}
-            else logWarn("No start date information, skipping");
-        if (padlockArguments.containsKey("de"))
-        {if (padlockString2Data(padlockArguments.get("ds")) == null) {logFatal("Error 5 - Incorrect date supplied");return 5;}}
-            else logWarn("No end date information, skipping");
-        if (padlockArguments.containsKey("vol"))
-        {try { volume = Integer.parseInt(padlockArguments.get("vol")); }
-            catch (NumberFormatException e) { logFatal("Error 5 - Incorrect volume supplied");return 5;}}
-            else logWarn("No volume information, skipping");
+        if (padlockArguments.containsKey ("ds")) {
+            if (padlockString2Data (padlockArguments.get ("ds")) == null) {
+                logFatal ("Error 5 - Incorrect date supplied");
+                return 5;
+            }
+        } else logWarn ("No start date information, skipping");
+        if (padlockArguments.containsKey ("de")) {
+            if (padlockString2Data (padlockArguments.get ("ds")) == null) {
+                logFatal ("Error 5 - Incorrect date supplied");
+                return 5;
+            }
+        } else logWarn ("No end date information, skipping");
+        if (padlockArguments.containsKey ("vol")) {
+            try {
+                volume = Integer.parseInt (padlockArguments.get ("vol"));
+            } catch (NumberFormatException e) {
+                logFatal ("Error 5 - Incorrect volume supplied");
+                return 5;
+            }
+        } else logWarn ("No volume information, skipping");
 
         // ШАГ 4: ???
         {/*https://www.youtube.com/watch?v=tO5sxLapAts*/}
--- src/test/java/groupPadlock/AppTest.java
+++ src/test/java/groupPadlock/AppTest.java
@@ -1,74 +1,36 @@
 package groupPadlock;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import static junit.framework.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase
-{
-     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-        public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+    App tester;
+    @Before
+    public void testBefore() throws Exception {
+    tester = new App ();
     }
-    /**
-     * @return the suite of tests being tested
-     */
-        public static Test suite()
-        {
-            return new TestSuite( AppTest.class );
-        }
-        /**
-         * Rigourous Test :-)
-         */
-        public void testApp()
-        {
-            assertTrue( true );
-        }
 
-
+    @Test
     public void testGo() throws Exception {
-        App tester = new App();
-        try {
-            int i=0;
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{"-h"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (1, tester.go (new String[]{"-login", "XXX", "-pass", "XXXX"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (2, tester.go (new String[]{"-login", "jdoe", "-pass", "XXXX"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (3, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "XXX", "-res", "a.b"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "XXX"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "WRITE", "-res", "a"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "WRITE", "-res", "a.bc"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "2015-01-01", "-de", "2015-12-31", "-vol", "100"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (5, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "01-01-2015", "-de", "2015-12-31", "-vol", "100"}));
-            System.out.println("-"+ (++i) +"---------------------");
-            assertEquals (5, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "2015-01-01", "-de", "2015-12-31", "-vol", "XXX"}));
-        }
-        catch (junit.framework.AssertionFailedError e) {System.out.println("Something went wrong");}
+        assertEquals (0, tester.go (new String[]{}));
+        assertEquals (0, tester.go (new String[]{"-h"}));
+        assertEquals (1, tester.go (new String[]{"-login", "XXX", "-pass", "XXXX"}));
+        assertEquals (2, tester.go (new String[]{"-login", "jdoe", "-pass", "XXXX"}));
+        assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ"}));
+        assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a"}));
+        assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b"}));
+        assertEquals (3, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "XXX", "-res", "a.b"}));
+        assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "XXX"}));
+        assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "WRITE", "-res", "a"}));
+        assertEquals (4, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "WRITE", "-res", "a.bc"}));
+        assertEquals (0, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "2015-01-01", "-de", "2015-12-31", "-vol", "100"}));
+        assertEquals (5, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "01-01-2015", "-de", "2015-12-31", "-vol", "100"}));
+        assertEquals (5, tester.go (new String[]{"-login", "jdoe", "-pass", "sup3rpaZZ", "-role", "READ", "-res", "a.b", "-ds", "2015-01-01", "-de", "2015-12-31", "-vol", "XXX"}));
     }
 }
 
